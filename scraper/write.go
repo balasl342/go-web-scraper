116dc0b6df24726c981b71e736a0f23aaac0d4f8
Bala C <balasl342@gmail.com>
2024-10-29 11:53:23 +0530

    Use helper function to check error cases

--- scraper/write.go
+++ scraper/write.go
@@ -3,18 +3,16 @@ package scraper
 
 import (
 	"encoding/csv"
-	"log"
 	"os"
 
 	"github.com/balasl342/go-web-scraper/models"
+	"github.com/balasl342/go-web-scraper/utils"
 )
 
 // WriteToCSV writes the scraped articles to a CSV file.
 func WriteToCSV(articles []models.Article, filename string) {
 	file, err := os.Create(filename)
-	if err != nil {
-		log.Fatalf("Failed to create CSV file: %v", err)
-	}
+	utils.CheckError("Failed to create CSV file: ", err)
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
